9e9b467a8ef52e89126232eedf0f273c1a8be367
fefe93 <frusche@mail.uni-mannheim.de>
2022-06-20 16:48:03 +0200

    initial commit

---
+++ trial_run.R
@@ -0,0 +1,21 @@
+# https://hollina.github.io/make-a-job-market-website.html
+library(blogdown)
+#blogdown::install_hugo(force = TRUE)
+blogdown::serve_site()
+# to end server:
+stop_server()
+
+# documentation: https://wowchemy.com/docs/
+
+# personal information: config/_default/params.toml
+
+# to adjust information in header:
+# content/authors/admin/index.md
+
+# information in about section:
+# content/authors/admin/
+
+# files (incl papers, : static/files/
+
+# to adjust own theme go to /themes/hugo-academic/data/themes/own.toml
+# see https://wowchemy.com/docs/getting-started/customization/#custom-theme
